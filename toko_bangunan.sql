8a7b71f40f39522c7585976895e91b6cb489773a
Joana I. Aldoranti <joanaaldoranti09@gmail.com>
2024-11-30 13:37:20 +0700

    Add files via upload

---
+++ toko_bangunan.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2024 at 07:36 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_bangunan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(8) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_admin` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_admin`) VALUES
+(1, 'admin', '1234', 'kita bersama');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `distributor`
+--
+
+CREATE TABLE `distributor` (
+  `id_distributor` int(11) NOT NULL,
+  `kode_distributor` varchar(20) NOT NULL,
+  `nama_distributor` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `distributor`
+--
+
+INSERT INTO `distributor` (`id_distributor`, `kode_distributor`, `nama_distributor`) VALUES
+(1, 'MB42647', 'PT. Maju Bersama'),
+(2, 'WI813197', 'PT. Wisma Indah'),
+(3, 'BI37621', 'PT. Bangun Indah'),
+(4, 'PB63927', 'PT. Permata Bakti'),
+(5, 'BN82740', 'PT. Bakti Negara');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `id_distributor` int(11) NOT NULL,
+  `nama_distributor` varchar(100) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `id_distributor`, `nama_distributor`, `kategori`, `stok`, `harga`) VALUES
+(3, 'kayu batang', 1, 'PT. Maju Bersama', 'kayu', 1000, 30000.00),
+(4, 'semen', 1, 'PT. Maju Bersama', 'semen', 10000, 45000.00),
+(5, 'besi batang', 1, 'PT. Maju Bersama', 'besi', 10000, 80000.00),
+(6, 'ddsdsd', 1, 'PT. Maju Bersama', 'dsdsds', 12, 12000.00),
+(7, 'pasir', 1, 'PT. Maju Bersama', 'pasir', 500, 40000.00),
+(8, 'fsss', 1, 'PT. Maju Bersama', 'sdsds', 100, 20000.00),
+(9, 'wswsw', 4, 'PT. Permata Bakti', 'swswsw', 20, 1000.00),
+(10, 'anu', 4, 'PT. Permata Bakti', 'nganu', 1, 2000.00),
+(11, 'dwdwd', 3, 'PT. Bangun Indah', 'dwdwd', 10, 1000.00),
+(13, 'paku', 1, 'PT. Maju Bersama', 'paku', 10000, 1000.00),
+(14, 'besi batang', 5, 'PT. Bakti Negara', 'besi', 50, 80000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksidetail`
+--
+
+CREATE TABLE `transaksidetail` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga_satuan` decimal(10,2) NOT NULL,
+  `subtotal` decimal(10,2) NOT NULL,
+  `tanggal_transaksi` datetime DEFAULT current_timestamp(),
+  `total_harga` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksidetail`
+--
+
+INSERT INTO `transaksidetail` (`id_transaksi`, `id_admin`, `id_produk`, `nama_produk`, `kategori`, `jumlah`, `harga_satuan`, `subtotal`, `tanggal_transaksi`, `total_harga`) VALUES
+(1, 1, 3, 'kayu batang', 'kayu', 5, 30000.00, 0.00, '2024-11-30 13:11:09', 0.00),
+(2, 1, 3, 'kayu batang', 'kayu', 10, 30000.00, 0.00, '2024-11-30 13:35:36', 0.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `distributor`
+--
+ALTER TABLE `distributor`
+  ADD PRIMARY KEY (`id_distributor`),
+  ADD UNIQUE KEY `kode_distributor` (`kode_distributor`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_distributor` (`id_distributor`);
+
+--
+-- Indexes for table `transaksidetail`
+--
+ALTER TABLE `transaksidetail`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_admin` (`id_admin`),
+  ADD KEY `id_produk` (`id_produk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `distributor`
+--
+ALTER TABLE `distributor`
+  MODIFY `id_distributor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `transaksidetail`
+--
+ALTER TABLE `transaksidetail`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_distributor`) REFERENCES `distributor` (`id_distributor`);
+
+--
+-- Constraints for table `transaksidetail`
+--
+ALTER TABLE `transaksidetail`
+  ADD CONSTRAINT `transaksidetail_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`),
+  ADD CONSTRAINT `transaksidetail_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
